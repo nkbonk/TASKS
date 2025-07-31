44fc478670de618f09ef3b3abbe213cce16223fe
nkbonk <nebonk846@gmail.com>
2025-07-31 18:48:22 +0500

    Create LeetCode_20.py

---
+++ LeetCode_20.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for symbol in s:                    
+            if symbol in '([{':             
+                stack.append(symbol)        
+            else:                           
+                if not stack:               
+                    return False
+                top = stack.pop()           
+                if (symbol == ')' and top != '(') or \
+                   (symbol == ']' and top != '[') or \
+                   (symbol == '}' and top != '{'):
+                    return False            
+
+        return not stack    
