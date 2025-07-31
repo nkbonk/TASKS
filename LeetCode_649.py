b3b68f327357d9852805f974eaf67111ea7606d4
nkbonk <nebonk846@gmail.com>
2025-07-31 18:48:56 +0500

    Create LeetCode_649.py

---
+++ LeetCode_649.py
@@ -0,0 +1,21 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        radiant = []
+        dire = []
+        n = len(senate)
+
+        for i in range(n):
+            if senate[i] == 'R':
+                radiant.append(i)
+            else:
+                dire.append(i)
+
+        while radiant and dire:
+            r = radiant.pop(0)
+            d = dire.pop(0)
+            if r < d:
+                radiant.append(r + n)
+            else:
+                dire.append(d + n)
+
+        return "Radiant" if radiant else "Dire"   # эту строчку написал ГПТ
