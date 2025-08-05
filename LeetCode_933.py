37ac7f0d02cdb0b1e9abae69ef8e24c37f65ff6e
nkbonk <nebonk846@gmail.com>
2025-08-05 20:59:40 +0500

    Update LeetCode_933.py

--- LeetCode_933.py
+++ LeetCode_933.py
@@ -1,10 +1,9 @@
-class RecentCounter:
-
     def __init__(self):
         self.calls = []
-
+        self.left = 0 
+    
     def ping(self, t: int) -> int:
         self.calls.append(t)
-        while self.calls[0] < t - 3000:
-            self.calls.pop(0)  
-        return len(self.calls)
+        while self.calls[self.left] < t - 3000:
+            self.left += 1
+        return len(self.calls) - self.left
