2bc305c359fd5286e4850e3a6fe088468e1c7d7e
nkbonk <nebonk846@gmail.com>
2025-08-10 12:01:39 +0500

    Delete LeetCode_933.py

--- LeetCode_933.py
+++
@@ -1,9 +0,0 @@
-    def __init__(self):
-        self.calls = []
-        self.left = 0 
-    
-    def ping(self, t: int) -> int:
-        self.calls.append(t)
-        while self.calls[self.left] < t - 3000:
-            self.left += 1
-        return len(self.calls) - self.left
