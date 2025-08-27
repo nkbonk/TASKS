b009170f2815f3a2dd5f6d096c2224506be3e471
nkbonk <nebonk846@gmail.com>
2025-08-27 23:15:57 +0500

    Update Queue_1.py

--- Queue/Queue_1.py
+++ Queue/Queue_1.py
@@ -5,61 +5,60 @@
 # dequeue() — удалить и вернуть первый элемент из очереди
 
 class QueueNode:
-    def __init__(self, value):
+    def __init__(self, value=None):
         self.value = value
         self.next = None
 
+
 class Queue:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.dummy = QueueNode()
+        self.head = self.dummy
+        self.tail = self.dummy
         self.size = 0
 
     def enqueue(self, i):
         new_node = QueueNode(i)
-        if not self.tail:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
+        self.tail.next = new_node 
+        self.tail = new_node  
         self.size += 1
 
     def dequeue(self):
-        if not self.head:
-            return None 
-            
-        val = self.head.value
-        self.head = self.head.next
-        if not self.head:
-            self.tail = None
+        if self.size == 0:
+            return None
+
+        first = self.head.next 
+        val = first.value
+        self.head.next = first.next
+        if self.tail == first:
+            self.tail = self.head 
         self.size -= 1
-        
         return val
 
 
 def test_queue():
     q = Queue()
-    assert q.size == 0 and q.head is None and q.tail is None
-    
+    assert q.size == 0 and q.head == q.dummy and q.tail == q.dummy
+
     q.enqueue(42)
-    assert q.size == 1 and q.head.value == 42 and q.tail.value == 42
+    assert q.size == 1 and q.head.next.value == 42 and q.tail.value == 42
     assert q.dequeue() == 42
-    assert q.size == 0 and q.head is None and q.tail is None
+    assert q.size == 0 and q.head == q.tail == q.dummy
 
     q.enqueue(1)
     q.enqueue(2)
     q.enqueue(3)
-    assert q.size == 3 and q.head.value == 1 and q.tail.value == 3
+    assert q.size == 3 and q.head.next.value == 1 and q.tail.value == 3
     assert q.dequeue() == 1 and q.dequeue() == 2 and q.dequeue() == 3
     assert q.dequeue() is None
-    
+
     q.enqueue(10)
     q.enqueue(20)
     assert q.dequeue() == 10
     q.enqueue(30)
-    assert q.size == 2 and q.head.value == 20 and q.tail.value == 30
+    assert q.size == 2 and q.head.next.value == 20 and q.tail.value == 30
     assert q.dequeue() == 20 and q.dequeue() == 30
 
 
 test_queue()
+print("тесты закончлисб")
