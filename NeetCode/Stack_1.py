cf6cb0c300f157be8c6231cdf2877b1d5769b456
nkbonk <nebonk846@gmail.com>
2025-08-29 23:22:13 +0500

    Update Stack_1.py

--- NeetCode/Stack_1.py
+++ NeetCode/Stack_1.py
@@ -2,7 +2,7 @@
 https://leetcode.com/problems/valid-parentheses/
 '''
 class Solution:
-    #  O(n)
+    #  O(n), тут много скобок надо проверять, поэтому меньше не выйдет
     def isValid(self, s: str) -> bool:
         if len(s) % 2 == 1:
             return False
