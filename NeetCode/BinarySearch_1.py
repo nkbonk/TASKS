3778eed0ad5e6e10bce6732de413a7d6fc702d1b
nkbonk <nebonk846@gmail.com>
2025-09-09 18:23:25 +0500

    Create BinarySearch_1.py

---
+++ NeetCode/BinarySearch_1.py
@@ -0,0 +1,21 @@
+'''
+https://leetcode.com/problems/binary-search/description/
+
+Time: O(log n). Это по условию задачи ниткода она должна работать за такое время
+Space: O(1)
+'''
+
+
+class Solution(object):
+    def search(self, nums, target):
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
